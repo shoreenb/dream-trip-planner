42d88d692577e872139f5066ae646fc88d8e63e4
shoreenb <shoreen.beckford@gmail.com>
2021-06-10 18:25:51 +0000

    Amend media queries for add, edit and itineraries pages

--- static/js/script.js
+++ static/js/script.js
@@ -144,3 +144,75 @@
 
      $(".btn-floating.red").click(duplicate);
  });
+
+ let counter1 = 1;
+
+ function duplicate() {
+     $("#card_c").after(`<div id="card_c${counter1}" class="row">
+                    <div class="card-panel col s12 m6 offset-m3">
+                        <div class="first_itinerary_card">
+                            <div class="row">
+                                <div class="input-field col s6 col s10 offset-m1 offset-m1">
+                                    <i class="fas fa-pencil-alt prefix light-blue-text text-accent-2"></i>
+                                    < input id = "activity_name"
+                                    name = "activity_name"
+                                    minlength = "5"
+                                    maxlength = "50"
+                                        type="text" class="validate" required>
+                                    <label for="activity_name">Activity Name</label>
+                                </div>
+                            </div>
+                            <!-- day -->
+                            <div class="input-field col s3 offset-m1">
+                                <i class="fas fa-calendar-day prefix light-blue-text text-accent-2"></i>
+                                < input id = "day"
+                                name = "day"
+                                type = "number"
+                                min = "1"
+                                class = "validate"
+                                required >
+                                <label for="day">Day</label>
+                            </div>
+                            <!-- timepicker -->
+                            <div class="input-field col s3">
+                                <i class="fas fa-clock prefix light-blue-text text-accent-2"></i>
+                                < input id = "time"
+                                name = "time"
+                                type = "text"
+                                class = "timepicker validate"
+                                required >
+                                <label for="time">Time</label>
+                            </div>
+                            <!-- duration of itinerary item -->
+                            <div class="input-field col s3">
+                                <i class="fas fa-stopwatch prefix light-blue-text text-accent-2"></i>
+                                < input id = "duration"
+                                name = "duration"
+                                type = "number"
+                                step = "10"
+                                min = "0"
+                                class = "validate"
+                                    required>
+                                <label for="duration">Duration(Minutes)</label>
+                            </div>
+                            <!-- itinerary item description -->
+                            <div class="row">
+                                <div class="input-field col s10 offset-m1 offset-m1">
+                                    <i class="fas fa-align-left prefix light-blue-text text-accent-2"></i>
+                                    < textarea id = "item_description"
+                                    name = "item_description"
+                                    minlength = "5"
+                                        maxlength="200" class="materialize-textarea validate" required></textarea>
+                                    <label for="item_description">Description</label>
+                                    <a class="btn-floating red btn-large right waves-effect waves-light red activator"
+                                        onclick="duplicate();"><i class="fas fa-plus"></i></a>
+                                        < a class = "btn-floating btn-large left waves-effect waves-light dark-blue activator"
+                                         onclick = "duplicate();" > < i class = "fas fa-minus" > < /i></a >
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>`);
+     counter1++;
+
+ }
